2697fe456e6e69e96f98499592d5f55b93b816e9
Leonardo Cespedes <leocspds01@gmail.com>
2023-07-03 01:52:00 -0500

    se gregan cambios a la aplicacion con el Hook personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,38 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import 'react-icons/fa'
 import Header from './Components/Header'
 import Task from './Components/Task'
 import TaskList from './Components/TaskList'
-import TaskEdit from './Components/TaskEdit'
-const TASKS = [
-  {
-    id: 1,
-    task: 'ir al gym'
-  },
-  {
-    id: 2,
-    task: 'ir de compras'
-  },
-  {
-    id: 3,
-    task: 'ir a la cita medica'
-  }
-];
+import { useTask } from './hooks/useTask'
+
 
 function App() {
-  const [todos, setTodos] = useState([]);
 
-  const addTask =(taskName) => {
-    console.log(`task add from ${taskName}`);
+  const {task,
+    taskCount,
+    pendingTaskCount,
+    handleNewTask,
+    handleDeleteTask,
+    handleCompleteTask,
+    handleUpdateTask} 
+    = useTask ();
 
-    let newTask = [...todos];
-
-    if (!todos. includes(taskName)) {
-      newTask = [...newTask, taskName];
-    } else {
-      newTask= newTask.filter((task) => taskName !=task);
-    }
-
-    setTodos(newTask);
-    localStorage.setItem("todos", JSON,stringify(newTask));
-  }
-
-  useEffect(() =>{
-    const localStorageData = localStorage.getItem("todos");
-    const storedTodos = JSON.parse(localStorageData);
-
-    setTodos(storedTodos);
-  },[])
-  
-  return (
+   return (
     <>
+    <div className='counter-todos'>
+      <h3>N° Tareas: {taskCount}</h3>
+      <h3>Pendientes: {pendingTaskCount}</h3>
+    </div>
       <div>
        <Header/>
-       <Task todos={TASKS}/>
-       <TaskEdit
-       key={TASKS.name}
-       name={TASKS.name}
-       onAddtask = {addTask}
+       <Task handleNewTask={handleNewTask}/>
+       <TaskList
+       task={task}
+       handleUpdateTask={handleUpdateTask}
+       handleDeleteTask={handleDeleteTask}
+       handleCompleteTask={handleCompleteTask}
+       
        />
-       <TaskList/>
 
       </div>
     </>
--- src/Components/TaskEdit.jsx
+++ src/Components/TaskEdit.jsx
@@ -1,24 +1,16 @@
 import { FaEdit } from "react-icons/fa";
 
-export default function TaskEdit ({taskName}) {
-    const addTask = () => {
-    onAddtask(taskName)
-    };
-    return (
-     <>
-      <h2>
-        {taskName}
-      </h2>
-     </>,
-     <form>
+export default function TaskEdit ({task, handleUpdateTask}) {
+     return (
+       <form>
        <input
        type='text'
        className='input-update'
        name="description"
-       value='estudiar ingles'
+       value= {task.description}
        placeholder="que hay que hacer"
        />,
-       <button onClick={addTask} className="btn-edit" type="submit"><FaEdit/></button>
+       <button className="btn-edit" type="submit"><FaEdit/></button>
        </form>         
     )
 }
