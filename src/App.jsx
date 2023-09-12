9e6424ea6ba68c70971ca9a83cdd238eab99f5ee
Leonardo Cespedes <leocspds01@gmail.com>
2023-09-11 22:38:01 -0500

    se agregan cambios en App.jsx, se actualiza el chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,32 @@ import Menu from './pages/Menu';
 import SobreNosotrosPage from './pages/SobreNosotrosPage';
 import TareaPage from './pages/TareaPage';
 import HomePage from './pages/Homepage';
+import { ChakraProvider, Box, Grid, GridItem } from '@chakra-ui/react';
 
-export function App() {
+function App() {
   return (
     <div>
+    <ChakraProvider>
       <BrowserRouter>
+      <Grid
+      templateAreas={`"Header Header" "main main"`}
+      gridTemplateRows={"70px 1fr 20px"}
+      gridAutoColumns={"120px 1fr"}
+      h={"150px"}
+      gap={"0.5"}
+      color={"#4A5568"}
+      fontWeight={"bold"}>
         <Menu />
+      <Box>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/sobre-nosotros' element={<SobreNosotrosPage />} />
           <Route path='/Tarea/:id' element={<TareaPage />} />
         </Routes>
+      </Box>
+      </Grid>
       </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
